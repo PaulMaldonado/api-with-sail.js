e214c1aaae851423f892b4d563de99ceb1cce5a3
JPaúlmn <paul_pastore10@hotmail.com>
2020-06-03 23:10:17 -0500

    Modificando la datastore

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://root@localhost:3306/task',
+    url: 'mysql://user@localhost:3306/test',
 
   },
 
